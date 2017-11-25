6ca1371123e46f3e70b4235fea80e0a53a279aff
Beverly Lau <beverly.lau@klarna.com>
2017-11-25 12:05:40 +0100

    Add function for square

--- src/main/scala/Test.scala
+++ src/main/scala/Test.scala
@@ -4,8 +4,14 @@ object One {
   val one = "hello"
   def main(args: Array[String]): Unit = {
     println("HELLOOOOOOO")
+    println("the answer is: " ++ square(2).toString)
+  }
+
+  def square(x: Int): Int = {
+    x * x
   }
 }
+
 
 object Two {
   val two: String = "2"
