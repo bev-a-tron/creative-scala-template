b1f427e7624e4fce74e3ded9af7d7a61d19c2e9f
Beverly Lau <beverly.lau@klarna.com>
2017-11-25 12:37:38 +0100

    Add imports and halve

--- src/main/scala/Test.scala
+++ src/main/scala/Test.scala
@@ -1,14 +1,25 @@
 package test
 
+import doodle.core._
+import doodle.core.Image._
+import doodle.syntax._
+import doodle.jvm.Java2DFrame._
+import doodle.backend.StandardInterpreter._
+
 object One {
   val one = "hello"
   def main(args: Array[String]): Unit = {
     println("HELLOOOOOOO")
     println("the answer is: " ++ square(2).toString)
+    println("the second answer is: " ++ halve(22).toString)
   }
 
   def square(x: Int): Int = {
     x * x
+  }
+
+  def halve(x: Double): Double = {
+    x/2.0
   }
 }
 
