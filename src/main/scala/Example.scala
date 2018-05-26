b8aa6943a8f9e0e9ec439c594c030a4f38e845a7
Beverly Lau <beverly.lau@klarna.com>
2018-05-26 17:45:45 +0200

    Refactor

--- src/main/scala/Example.scala
+++ src/main/scala/Example.scala
@@ -10,27 +10,27 @@ import doodle.backend.StandardInterpreter._
 
 object Example {
 
-  val roof = Image.triangle(50, 30).lineWidth(0).fillColor(Color.brown)
-  val wall = Image.rectangle(50, 20).lineWidth(0).fillColor(Color.red) above
-    (Image.rectangle(20, 30).lineWidth(0).fillColor(Color.red) beside
-      Image.rectangle(10, 30).lineWidth(0).fillColor(Color.black) beside
-      Image.rectangle(20, 30).lineWidth(0).fillColor(Color.red))
+  val roof = Image.triangle(50, 30).fillColor(Color.brown)
+  val wall = Image.rectangle(50, 20).fillColor(Color.red) above
+    (Image.rectangle(20, 30).fillColor(Color.red) beside
+      Image.rectangle(10, 30).fillColor(Color.black) beside
+      Image.rectangle(20, 30).fillColor(Color.red))
 
   val house = roof above wall
 
-  val tree = Image.circle(30).lineWidth(0).fillColor(Color.green) above
-    Image.rectangle(10, 20).lineWidth(0).fillColor(Color.brown)
+  val tree = Image.circle(30).fillColor(Color.green) above
+    Image.rectangle(10, 20).fillColor(Color.brown)
 
   val house_and_tree = house beside tree
 
-  val halfstreet = (Image.rectangle(30, 2).lineWidth(0).fillColor(Color.yellow) beside
-    Image.rectangle(10, 2).lineWidth(0).fillColor(Color.black)) above
-    Image.rectangle(40,4).lineWidth(0).fillColor(Color.black)
+  val halfstreet = (Image.rectangle(30, 2).fillColor(Color.yellow) beside
+    Image.rectangle(10, 2).fillColor(Color.black)) above
+    Image.rectangle(40,4).fillColor(Color.black)
 
   val street = halfstreet beside halfstreet beside halfstreet beside halfstreet
   val one_house = house_and_tree above street
 
-  val three_houses = one_house beside one_house beside one_house
+  val three_houses = (one_house beside one_house beside one_house) lineWidth(0)
 
   three_houses.draw
 }
